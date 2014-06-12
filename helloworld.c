383cb36ebd3b113975c2997898a093190e5b9523
tarun <tarun@tarun-Inspiron-N4050.(none)>
2014-06-12 12:58:16 +0530

    This consist of a subtact and multiply function

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main() {
+	int a=1,b=2;
 	printf("Hello World!\n");
-	printf("The sum is %d",add(1,2));
+	printf("The sum is %d\n",add(1,2));
 	swap(&a, &b);
+	printf("The diff is %d\n",sub(2,1));
+	printf("The multiplication is %d\n",multi(2,1));
 	return 0;
 	
  }
@@ -16,4 +19,12 @@ int swap(int *a, int *b) {
 	*a = *a + *b;
 	*b = *a -*b;
 	*a = *a - *b;
+}
+int sub(int a ,int b)
+{
+	return (a-b);
+}
+int multi(int a ,int b)
+{
+	return(a*b);
 }
