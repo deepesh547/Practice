71ed73c394da16782287c5ac383573c94b5452cd
binora <findbinnyarora@gmail.com>
2014-06-13 00:24:42 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -2,11 +2,18 @@
 int main() {
 	printf("Hello World!\n");
 	printf("The sum is %d",add(1,2));
+	swap(&a, &b);
 	return 0;
 	
  }
-// TODO: Add a second program addition.c to add two numbers a and b
+
 int add(int a,int b)
 {
 	return (a+b);
+}
+
+int swap(int *a, int *b) {
+	*a = *a + *b;
+	*b = *a -*b;
+	*a = *a - *b;
 }
