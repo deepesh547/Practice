dbde1f4e3312382f82590397cbb38c659c73b199
tarun <tarun@tarun-Inspiron-N4050.(none)>
2014-06-12 10:03:50 +0530

    first commit

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main() {
-	printf("Hello World!");
+	printf("Hello World!\n");
+	printf("The sum is %d",add(1,2));
 	return 0;
+	
  }
 // TODO: Add a second program addition.c to add two numbers a and b
-
+int add(int a,int b)
+{
+	return (a+b);
+}
