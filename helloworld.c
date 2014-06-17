a7e787c47f3fa02ff67245cfdb380355eb50d5ca
binora <findbinnyarora@gmail.com>
2014-06-17 23:30:43 +0530

    added divide function

--- helloworld.c
+++ helloworld.c
@@ -6,7 +6,9 @@ int main() {
 	swap(&a, &b);
 	printf("The diff is %d\n",sub(2,1));
 	printf("The multiplication is %d\n",multi(2,1));
+	printf("the division is %d", divide(a, b));
 	return 0;
+	
 	
  }
 
@@ -27,4 +29,12 @@ int sub(int a ,int b)
 int multi(int a ,int b)
 {
 	return(a*b);
+}
+// a/b
+int  divide(int a, int b) {
+	if (b != 0) {
+		return a/b;
+	} else {
+		printf("unable to divide!")
+	}
 }
